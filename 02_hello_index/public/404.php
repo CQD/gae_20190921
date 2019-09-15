e5e29eabbbe75dc56a20ec25d0ab55b0101a28b7
CQD <cqd@hiigara.net>
2019-09-15 21:23:27 +0800

    讓範例 02 的 404 訊息短一點
    這樣展示放大畫面時訊息被切掉的機會比較小

--- 02_hello_index/public/404.php
+++ 02_hello_index/public/404.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/navbar.php';
 
 http_response_code(404);
-printf('[%s] 這是個 404 頁面，你想看的網址「%s」我找不到',
+printf('[%s] 這是個 404 頁面，我找不到「%s」',
     date('Y-m-d H:i:s'),
     $_SERVER['REQUEST_URI']
 );
